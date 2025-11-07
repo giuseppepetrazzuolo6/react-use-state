123e3182cb09bc2817754ad5ca69e34e19f5bb1e
giuseppepetrazzuolo6 <giuseppepetrazzuolo6@gmail.com>
2025-11-07 12:04:53 +0100

    create layout markup

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,25 @@ function App() {
 
   return (
     <>
+      <div className="container d-flex justify-content-center align-items-center vh-100">
+        <div className="card rounded-0">
+          <div className="card-header bg-light">
+            <h1 className="py-2">Learn Web development</h1>
+          </div>
+          <div className="card-body p-5">
+            <button className="btn btn-primary">HTML</button>
+            <button className="btn btn-primary">CSS</button>
+            <button className="btn btn-primary">JavaScript</button>
+            <button className="btn btn-primary">Node.js</button>
+            <button className="btn btn-primary">Express</button>
+            <button className="btn btn-primary">React.js</button>
+            <div className="border border-1 border-dark rounded-3 p-3 my-4">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat reiciendis, amet aliquid aut hic tempore laborum quae nobis? Optio, odit.</p>
+            </div>
+          </div>
+        </div>
 
+      </div>
     </>
   )
 }
